6eb1da971cf2f8e55f71eec2fbe947b4871b96c7
saurabh-kayasth <i.am.saurabh.kayasth@gmail.com>
2020-12-06 16:14:19 +0530

    first commit

---
+++ Arrays/MajorityElementArray.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class MajorityElementArray {
+    public static void findMajorityElement(int[] arr) {
+        HashMap<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (countMap.containsKey(arr[i])) {
+                int count = countMap.get(arr[i]) + 1;
+                if (count > arr.length / 2) {
+                    System.out.println(arr[i]);
+                    return;
+                }
+                countMap.put(arr[i], count);
+            } else {
+                countMap.put(arr[i], 1);
+            }
+        }
+        System.out.println("Majority element does not exist");
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[] { 2, 2, 2, 2, 3, 3, 3, 3, 3 };
+        findMajorityElement(arr);
+    }
+}
---
+++ Arrays/RotateArray.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class RotateArray{
+    public static void name() {
+        System.out.println("hello");
+    }
+}
