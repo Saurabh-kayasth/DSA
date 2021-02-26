819d823b1ff37886a5103f22269327ceeb6f41b4
saurabh-kayasth <i.am.saurabh.kayasth@gmail.com>
2021-02-26 23:36:31 +0530

    added reverse string using loops

---
+++ Loops/ReverseString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string : ");
+        String str= sc.next();
+
+        String revStr = "";
+
+        for(int i = str.length()-1;i>=0;i--){
+            revStr += str.charAt(i);
+        }
+
+        System.out.println("Reversed string : "+revStr);
+        sc.close();
+    }
+}
