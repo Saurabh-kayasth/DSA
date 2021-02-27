8a572bc64faf2ea4dadd29acc7f990abb01ed28d
saurabh-kayasth <i.am.saurabh.kayasth@gmail.com>
2021-02-28 02:19:53 +0530

    ARRAY : Hours Glass Sum

---
+++ Arrays/HoursGlass.java
@@ -0,0 +1,29 @@
+public class HoursGlass {
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{
+            {1,1,1,0,0,0},
+            {0,1,0,0,0,0},
+            {1,1,1,0,0,0},
+            {0,0,2,4,4,0},
+            {0,0,0,2,0,0},
+            {0,0,1,2,4,0}
+        };
+
+        System.out.println(hourglassSum(arr));
+    }
+
+
+    static int hourglassSum(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0;i<4;i++){
+            for(int j = 0;j<4;j++){
+                int sum = arr[i][j] + arr[i][j+1]+arr[i][j+2] + arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+                if(sum > max){
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+}
