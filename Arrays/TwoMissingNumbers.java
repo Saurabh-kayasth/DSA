260dda0a379f0b34a346e9fe578cc1f66c121553
saurabh-kayasth <i.am.saurabh.kayasth@gmail.com>
2021-03-02 20:41:19 +0530

    ARRAY : Find two consecutive missing numbers ( O(n) space )

---
+++ Arrays/TwoMissingNumbers.java
@@ -0,0 +1,34 @@
+public class TwoMissingNumbers {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int[] ans = findFirstTwoMissingNumbers(nums);
+        for(int i=0;i<=ans.length-1;i++){
+            System.out.println(ans[i] + " ");
+        }
+    }
+
+    static int[] findFirstTwoMissingNumbers(int[] nums){
+
+        int[] ans = new int[nums.length+2];
+        for(int i = 0; i<=nums.length-1; i++){
+            if(nums[i] > 0){
+                if( nums[i] <= nums.length+2){
+                    ans[nums[i]-1] = nums[i];
+                }
+            }
+        }
+
+        int[] twoNumbers = new int[2]; 
+        int counter = 0;
+
+        for(int i=0;i<ans.length;i++){
+            // System.out.println(ans[i]);
+
+            if(ans[i] == 0){
+                twoNumbers[counter++] = i+1;
+            } 
+        }
+
+        return twoNumbers;
+    }
+}
