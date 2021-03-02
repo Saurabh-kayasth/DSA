be7dbc032301f87ec2e9d797d0ec01513a502e33
saurabh-kayasth <i.am.saurabh.kayasth@gmail.com>
2021-03-02 12:22:37 +0530

    ARRAY : Array product except itselfs

---
+++ Arrays/InversionArray.java
@@ -0,0 +1,11 @@
+public class InversionArray {
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 2, 1};
+        System.out.println(inversionInArray(arr));
+    }
+
+    static int inversionInArray(int[] arr){
+        
+        return 0;
+    }
+}
