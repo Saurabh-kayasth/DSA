895760c44df15e5f6472e03fbfb7323fc2138b53
saurabh-kayasth <i.am.saurabh.kayasth@gmail.com>
2021-03-11 20:23:47 +0530

    Recursion : Taylor series using Horner's rule

--- Recursion/TaylorSeries.java
+++ Recursion/TaylorSeries.java
@@ -3,6 +3,7 @@ public class TaylorSeries {
         int x = 1;
         int n = 10;
         System.out.println(e(x,n));
+        System.out.println(eUsingHorner(x,n));
     }
 
     static double p=1.0,f=1.0;
@@ -15,6 +16,17 @@ public class TaylorSeries {
             p = p*x;
             f = f*n;
             return r + p/f; 
+        }
+    }
+
+    static double S=1;
+    static double eUsingHorner(double x, double n){
+        if(n == 0){
+            return S;
+        }
+        else{
+            S = 1+(x/n)*S;
+            return eUsingHorner(x, n-1);
         }
     }
 }
